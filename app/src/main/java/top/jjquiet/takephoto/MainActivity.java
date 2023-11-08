11b0b212837e510c6e3a6729b9e4b46a054a8eb5
JiangJianqi <jiangjianqi777@gmail.com>
2023-11-08 21:20:29 +0800

    feat 拍照与查看 bug 二次跳转SecondFragment出错

--- app/src/main/java/top/jjquiet/takephoto/MainActivity.java
+++ app/src/main/java/top/jjquiet/takephoto/MainActivity.java
@@ -1,10 +1,21 @@
 package top.jjquiet.takephoto;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
-import com.google.android.material.snackbar.Snackbar;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.core.content.FileProvider;
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
-import androidx.core.view.WindowCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -13,12 +24,24 @@ import top.jjquiet.takephoto.databinding.ActivityMainBinding;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.Manifest;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Locale;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
-
+    private static final int REQUEST_CAMERA_PERMISSION = 101;
+    private static final int REQUEST_IMAGE_CAPTURE = 102;
+    private Uri photoURI;
+    private ActivityResultLauncher<Uri> takePictureLauncher;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,42 +55,105 @@ public class MainActivity extends AppCompatActivity {
         appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
 
+        // 初始化ActivityResultLauncher
+        takePictureLauncher = registerForActivityResult(new ActivityResultContracts.TakePicture(), new ActivityResultCallback<Boolean>() {
+            @Override
+            public void onActivityResult(Boolean result) {
+                if (result) {
+                    Bundle bundle = new Bundle();
+                    bundle.putString("photoUri", photoURI.toString());
+                    savePhotoUri(photoURI);
+                    Log.d("蒋建琪", "onActivityResult: " + photoURI.toString());
+//                    SecondFragment secondFragment = new SecondFragment();
+//                    secondFragment.setArguments(bundle);
+                    if(navController.getCurrentDestination().getId() == R.id.FirstFragment){
+                        navController.navigate(R.id.action_FirstFragment_to_SecondFragment, bundle);
+                    }
+                } else {
+                    // 用户取消或拍摄失败
+                }
+            }
+        });
         binding.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAnchorView(R.id.fab)
-                        .setAction("Action", null).show();
+                if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                    requestPermissions(new String[]{Manifest.permission.CAMERA}, REQUEST_CAMERA_PERMISSION);
+                } else {
+                    // Permission has already been granted
+                    openCamera();
+                }
             }
         });
     }
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
+        } else if (id == R.id.action_album) {
+            NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+            if (navController.getCurrentDestination().getId() == R.id.FirstFragment) {
+                navController.navigate(R.id.action_FirstFragment_to_SecondFragment);
+                return true;
+            }
         }
 
         return super.onOptionsItemSelected(item);
     }
-
     @Override
     public boolean onSupportNavigateUp() {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         return NavigationUI.navigateUp(navController, appBarConfiguration)
                 || super.onSupportNavigateUp();
     }
+    private void openCamera() {
+            File photoFile = createImageFile();
+            if (photoFile != null) {
+                photoURI = FileProvider.getUriForFile(this,
+                        "top.jjquiet.takephoto.file-provider",
+                        photoFile);
+                takePictureLauncher.launch(photoURI);
+                savePhotoUri(photoURI);
+            }
+    }
+    private File createImageFile()  {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmm", Locale.getDefault()).format(new Date());
+        String imageFileName = timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = null;
+        try {
+            image = File.createTempFile(
+                    imageFileName,  /* prefix */
+                    ".jpg",   /* suffix */
+                    storageDir      /* directory */
+            );
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return image;
+    }
+    private void savePhotoUri(Uri photoUri) {
+        SharedPreferences sharedPrefs = getSharedPreferences("PhotoUris", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPrefs.edit();
+
+        // 获取已存储的照片Uri列表
+        Set<String> photoUriSet = new HashSet<>(sharedPrefs.getStringSet("photoUris", new HashSet<>()));
+        // 将新的照片Uri添加到列表中
+        photoUriSet.add(photoUri.toString());
+        // 保存更新后的列表
+        editor.putStringSet("photoUris", photoUriSet);
+        editor.apply();
+    }
+
 }
--- app/src/main/java/top/jjquiet/takephoto/SecondFragment.java
+++ app/src/main/java/top/jjquiet/takephoto/SecondFragment.java
@@ -1,33 +1,56 @@
 package top.jjquiet.takephoto;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.content.SharedPreferences;
 
+import android.widget.ImageView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import com.bumptech.glide.Glide;
 import top.jjquiet.takephoto.databinding.FragmentSecondBinding;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+import static android.content.Context.MODE_PRIVATE;
+
 public class SecondFragment extends Fragment {
-
     private FragmentSecondBinding binding;
-
+    private ImageView imageView;
+    private RecyclerView recyclerView;
+    private PhotoAdapter photoAdapter;
+    private ArrayList<Uri> photoList = new ArrayList<>();
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
     ) {
-
         binding = FragmentSecondBinding.inflate(inflater, container, false);
+        recyclerView = binding.recyclerView;
+        photoList = loadPhotoUris();
+        Log.d("蒋建琪", "onCreateView photoSize: " + photoList.size());
+        photoAdapter = new PhotoAdapter(photoList);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setAdapter(photoAdapter);
         return binding.getRoot();
-
     }
-
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
+//        ImageView imageView = view.findViewById(R.id.CameraImageView);
+//        if (getArguments() != null && getArguments().containsKey("photoUri")) {
+//            Uri imageUri = Uri.parse(getArguments().getString("photoUri"));
+//            imageView.setImageURI(imageUri); // Set the ImageView to display the image
+//        }
         binding.buttonSecond.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -36,7 +59,56 @@ public class SecondFragment extends Fragment {
             }
         });
     }
-
+    public class PhotoAdapter extends RecyclerView.Adapter<PhotoAdapter.PhotoViewHolder> {
+        private ArrayList<Uri> photoList;
+        public PhotoAdapter(ArrayList<Uri> photos) {
+            photoList = photos;
+        }
+        @NonNull
+        @Override
+        public PhotoViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.photo_item, parent, false);
+            return new PhotoViewHolder(view);
+        }
+        @Override
+        public void onBindViewHolder(@NonNull PhotoViewHolder holder, int position) {
+            Uri photoUri = photoList.get(position);
+            Log.d("蒋建琪", "onBindViewHolder photoUri: " + photoUri.toString());
+//            holder.imageView.setImageURI(photoUri);
+            // 使用Glide来加载图片
+            Glide.with(holder.imageView.getContext())
+                    .load(photoUri)
+                    .into(holder.imageView);
+            Log.d("蒋建琪", "onBindViewHolder: " + photoUri.toString());
+        }
+        @Override
+        public int getItemCount() {
+            return photoList.size();
+        }
+        private class PhotoViewHolder extends RecyclerView.ViewHolder {
+            ImageView imageView;
+            public PhotoViewHolder(@NonNull View itemView) {
+                super(itemView);
+                imageView = itemView.findViewById(R.id.image_view);
+            }
+        }
+    }
+    // 更新照片列表的方法
+    public void addPhoto(Uri photoUri) {
+        photoList.add(0, photoUri); // 将新照片添加到列表的开头
+        photoAdapter.notifyItemInserted(0); // 通知Adapter插入操作
+    }
+    private ArrayList<Uri> loadPhotoUris() {
+        SharedPreferences sharedPrefs = getActivity().getSharedPreferences("PhotoUris", MODE_PRIVATE);
+        Set<String> photoUriSet = sharedPrefs.getStringSet("photoUris", new HashSet<>());
+        ArrayList<Uri> photoUris = new ArrayList<>();
+        for (String uriString : photoUriSet) {
+            photoUris.add(Uri.parse(uriString));
+        }
+        // 可能需要根据实际情况对Uri列表进行排序，以保证新拍的照片在上面
+        Collections.reverse(photoUris);
+        return photoUris;
+    }
     @Override
     public void onDestroyView() {
         super.onDestroyView();
